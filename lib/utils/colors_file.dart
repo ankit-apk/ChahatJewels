47e3d96c9d714ed15517c30bd095850ce00f5be8
Ankit <ankitsrivastava858@gmail.com>
2021-05-27 22:31:12 +0530

    Update

--- lib/utils/colors_file.dart
+++ lib/utils/colors_file.dart
@@ -1,8 +1,8 @@
 import 'dart:ui';
 
 class AppColors {
-  static const Color goldDark = const Color(0xffD1AF00);
   static const Color goldLite = const Color(0xffD1AF28);
+  static const Color goldDark = const Color(0xffD1AF00);
   static const Color fontColor = const Color(0xff2c5530);
   static const Color marginColor = const Color(0xffD1AF68);
   static const Color red = const Color(0xffFF0000);
